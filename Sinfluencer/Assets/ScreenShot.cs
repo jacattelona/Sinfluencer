cb9e5b5765d4f64fcc83b5b677b97165da50361d
Unknown <jacattelona@wpi.edu>
2019-09-27 21:23:28 -0400

    Camera Flash

---
+++ Sinfluencer/Assets/ScreenShot.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class ScreenShot : MonoBehaviour
+{
+    string filePath = "C:\\Users\\Jordan\\Desktop\\TestName.png";
+    public GameObject canvas;
+    public CanvasGroup cg;
+
+    bool flash = false;
+    float delay = 0;
+
+    enum Flash
+    {
+        None,
+        Delay,
+        Flash
+    }
+
+    Flash state = Flash.None;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(Application.persistentDataPath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ScreenCapture.CaptureScreenshot(filePath);
+            print("Printed screenshot");
+
+            //state = Flash.Delay;
+            flash = true;
+            cg.alpha = 0.0f;
+            delay = 0;
+        }
+
+        if (flash)
+        {
+            if (delay < .1)
+            {
+                delay += Time.deltaTime;
+                if (delay >= .1)
+                    cg.alpha = 1.0f;
+            }
+            else
+            {
+                cg.alpha -= Time.deltaTime * .85f;
+                if (cg.alpha <= 0)
+                    flash = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Texture2D tex = null;
+            byte[] fileData;
+
+            if (File.Exists(filePath))
+            {
+                fileData = File.ReadAllBytes(filePath);
+                tex = new Texture2D(2, 2);
+                tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+
+                canvas.GetComponent<Renderer>().material.mainTexture = tex;
+            }
+
+            print("Rendered Screenshot");
+        }
+    }
+}
